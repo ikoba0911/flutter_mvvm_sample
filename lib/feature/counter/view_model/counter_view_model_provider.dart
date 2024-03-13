8596b91d8bc95e27c9d803e622a55dce23230b9e
ikoba0911 <ikki.5884.dev@gmail.com>
2024-03-14 00:08:54 +0900

    delete view_model_provider

--- lib/feature/counter/view_model/counter_view_model_provider.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter_mvvm_sample/feature/counter/view_model/counter_view_model.dart';
-// import 'package:flutter_mvvm_sample/feature/counter/view_model/counter_view_state.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// final counterViewModelProvider =
-//     StateNotifierProvider.autoDispose<CounterViewModel, CounterViewState>(
-//         (ref) {
-//   return CounterViewModel();
-// });
